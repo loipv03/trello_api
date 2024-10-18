8beb6d7fdf7c84fababdd47728e489fa40718db1
phanvanloi <loipvph24552@fpt.edu.vn.com>
2024-10-18 19:30:39 +0700

    update

--- src/controller/auth/login.ts
+++ src/controller/auth/login.ts
@@ -36,14 +36,14 @@ const login = async (req: AuthenticatedRequest, res: Response, next: NextFunctio
 
         res.cookie('access_token', access_token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV !== 'production',
-            sameSite: 'strict'
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'none'
         });
 
         res.cookie('refresh_token', refresh_Token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV !== 'production',
-            sameSite: 'strict'
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'none'
         });
 
         return res.status(200).json({
