86959f9af678e72a5f25fb38c58f91adf6a920a9
phanvanloi <loipvph24552@fpt.edu.vn.com>
2024-10-18 20:39:33 +0700

    update

--- src/controller/auth/login.ts
+++ src/controller/auth/login.ts
@@ -36,14 +36,14 @@ const login = async (req: AuthenticatedRequest, res: Response, next: NextFunctio
 
         res.cookie('access_token', access_token, {
             httpOnly: true,
-            secure: false,
-            sameSite: 'lax'
+            secure: true,
+            sameSite: 'none'
         });
 
         res.cookie('refresh_token', refresh_Token, {
             httpOnly: true,
-            secure: false,
-            sameSite: 'lax'
+            secure: true,
+            sameSite: 'none'
         });
 
         return res.status(200).json({
