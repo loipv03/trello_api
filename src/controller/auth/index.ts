d63f934cf2c6c187076ba50e8fa05d8ad6bd2680
phanvanloi <loipvph24552@fpt.edu.vn.com>
2024-10-25 17:58:40 +0700

    update

--- src/controller/auth/index.ts
+++ src/controller/auth/index.ts
@@ -3,7 +3,7 @@ import searchUser from './search';
 import login from './login';
 import logout from './logout';
 import signup from './signup';
-import uploadAvatar from './uploadAvatar';
 import checkLogin from './checkLogin';
+import uploadAvatar from './uploadAvatar';
 
 export const authControlllers = { signup, login, logout, activateUser, uploadAvatar, searchUser, checkLogin }
